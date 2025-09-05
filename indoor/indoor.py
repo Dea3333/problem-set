814db247782c415c61b50fc7384bd3b3521cc8fb
Dea3333 <deakipiani06@gmail.com>
2025-09-05 16:29:12 +0400

    Update indoor.py

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,6 +1,14 @@
 def main():
-    text = input()
-    print(text.lower())
+    
+    greeting = input("Greeting: ").strip().lower()
+
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
 
 if __name__ == "__main__":
     main()
